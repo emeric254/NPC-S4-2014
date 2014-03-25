3e3c6211e912f06be14eb76c2482e673b1ac510b
Tosi Emeric <emeric254@live.fr>
2014-03-25 17:56:26 +0100

    modifié:         TP4/ex2/Broadcast.java

--- TP4/ex2/Broadcast.java
+++ TP4/ex2/Broadcast.java
@@ -1,8 +1,19 @@
 public class Broadcast extends IBroadcast <T> {
+    private int laule=0;
+    private int currentlaule=0;
+    private T objectsended;
+
     public void Subscribe(){
+        laule++;
     }
-    public void Send(T i) throws InterruptedException{
+    public synchronized void Send(T i) throws InterruptedException{
+        objectsended=i;
+        while(laule>currentlaule) try{sleep(500);}catch(Exception e){}
+        notifyAll();
     }
-    public T Receive() throws InterruptedException{
+    public synchronized T Receive() throws InterruptedException{
+        currentlaule++;
+        wait();
+        return objectsended;
     }
 }
