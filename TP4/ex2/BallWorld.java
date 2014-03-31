fafe9853adc65621a986090931a4d8ad5cd1a812
sidya82 <quentin.rouland.82@gmail.com>
2014-03-31 15:53:36 +0200

    nouveau fichier: TP4/ex2/Ball.java 	nouveau fichier: TP4/ex2/BallWorld.java 	nouveau fichier: TP4/ex2/BallsApp.java 	modifié:         TP4/ex2/Broadcast.java

---
+++ TP4/ex2/BallWorld.java
@@ -0,0 +1,70 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class BallWorld extends JPanel {
+
+    // default window size
+    private final int xSize = 250;
+    private final int ySize = 250;
+    public Broadcast broadcast ;
+    public ColorMaster laule;
+
+    private final static Color BGCOLOR = Color.white;
+
+    // a BallWorld contains Ball instances
+    private ArrayList<Ball> balls = new ArrayList<Ball>();
+
+    public BallWorld() {
+        setPreferredSize(new Dimension(xSize, ySize));
+        setOpaque(true);
+        setBackground(BGCOLOR);
+        broadcast = new Broadcast();
+        laule = new ColorMaster(this);
+    }
+
+    // This method is run from the GUI thread when the window needs redrawing
+    // It reads the balls array.
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (Ball b : balls)
+            b.draw(g);
+    }
+
+    public void addBall(final Ball b) {
+        SwingUtilities.invokeLater(new Runnable() {
+            // In order to access the balls array only from one thread,
+            // let this code be run from the GUI thread.
+            public void run() {
+                balls.add(b);
+                repaint();
+            }
+        });
+    }
+}
+
+
+class ColorMaster extends Thread {
+    private BallWorld monde;
+    public ColorMaster (BallWorld mondeDesBalles){
+        monde = mondeDesBalles;
+        start();
+    }
+    public synchronized void run() {
+            while(true)
+            {
+                try { sleep(3000); } catch (Exception e) { }
+                int R = (int)(Math.random()*255);
+                int G = (int)(Math.random()*255);
+                int B = (int)(Math.random()*255);
+                Color color = new Color(R, G, B);
+                try {monde.broadcast.Send(color);}catch(Exception e){}
+            }
+
+
+    }
+}
+
+
+
--- TP4/ex2/Broadcast.java
+++ TP4/ex2/Broadcast.java
@@ -1,19 +1,25 @@
-public class Broadcast extends IBroadcast <T> {
+public class Broadcast implements IBroadcast <Object> {
     private int laule=0;
     private int currentlaule=0;
-    private T objectsended;
+    private Object objectsended;
 
     public void Subscribe(){
         laule++;
     }
-    public synchronized void Send(T i) throws InterruptedException{
+    public void Send(Object i) throws InterruptedException{
         objectsended=i;
-        while(laule>currentlaule) try{sleep(500);}catch(Exception e){}
-        currentlaule=0;
+        while(laule>currentlaule)
+        {
+            //~ try{Thread.sleep(500);}catch(Exception e){}
+            System.err.println("currentlaule : " + currentlaule );
+            System.err.println("laule : " + laule );
+        }
         notifyAll();
+        currentlaule=0;
+
     }
-    public synchronized T Receive() throws InterruptedException{
-        currentlaule++;
+    public synchronized Object Receive() throws InterruptedException{
+        currentlaule+=1;
         wait();
         return objectsended;
     }
